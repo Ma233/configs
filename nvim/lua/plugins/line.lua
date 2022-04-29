490e204712bddd5f39ea81ba0f03a7eb89e1b532
magine <magine233@gmail.com>
2022-04-29 14:35:36 +0800

    show relative path in lualine

--- nvim/lua/plugins/line.lua
+++ nvim/lua/plugins/line.lua
@@ -2,4 +2,8 @@ require('lualine').setup({
     options = {
         theme = 'dracula-nvim',
     },
+    extensions = { 'fzf' },
+    sections = {
+        lualine_c = { { 'filename', file_status = true, path = 1, shorting_target = 40 } },
+    },
 })
