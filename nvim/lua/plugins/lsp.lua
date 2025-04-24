aee1669dfc7e7b06ec0eb3b74daa2351ae04fcd7
magine <magine233@gmail.com>
2025-04-24 18:54:06 +0800

    update lazy

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,7 +1,15 @@
 return {
-  "neovim/nvim-lspconfig",
-  init = function()
-    local keys = require("lazyvim.plugins.lsp.keymaps").get()
-    keys[#keys + 1] = { "<leader>rn", vim.lsp.buf.rename, desc = "Code Rename" }
-  end,
+  {
+    "neovim/nvim-lspconfig",
+    init = function()
+      local keys = require("lazyvim.plugins.lsp.keymaps").get()
+      keys[#keys + 1] = { "<leader>rn", vim.lsp.buf.rename, desc = "Code Rename" }
+    end,
+    opts = {
+      -- make sure mason installs the server
+      servers = {
+        bufls = {},
+      },
+    },
+  },
 }
