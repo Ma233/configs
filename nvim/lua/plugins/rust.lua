74b3c4495c612cf7ffd0b51d26354a44f047755f
magine <magine233@gmail.com>
2026-02-23 13:22:19 +0800

    update lazy plugins

--- nvim/lua/plugins/rust.lua
+++ nvim/lua/plugins/rust.lua
@@ -24,7 +24,7 @@ return {
         procMacro = {
           enable = true,
           ignored = {
-            ["async-trait"] = { "async_trait" },
+            -- ["async-trait"] = { "async_trait" },
             ["napi-derive"] = { "napi" },
             ["async-recursion"] = { "async_recursion" },
           },
